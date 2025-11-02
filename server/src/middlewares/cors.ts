50299fca4396136bd9af0d1f2c6c726916b3d178
iqbalshaikh <shaikhiqbal80004@gmail.com>
2025-11-02 13:26:20 +0530

    backend erro resolved occuring during deployment

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL, 'https://expense-tracker-app-pp6k.vercel.app'] 
+    ? ['https://expense-tracker-app-pp6k.vercel.app'] 
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
