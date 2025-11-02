7713fe50f1edf4615fd12619cc8d2df84d2ebf08
iqbalshaikh <shaikhiqbal80004@gmail.com>
2025-11-02 14:34:34 +0530

    added hard code client deploy url

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL || 'https://expense-tracker-app-pp6k.vercel.app'] 
+    ? ['https://expense-tracker-app-pp6k.vercel.app', process.env.FRONTEND_URL].filter(Boolean) as string[]
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
