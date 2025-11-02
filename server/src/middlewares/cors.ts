f51a2fd3c554ce371e7f774c4cc1071bde3f556b
iqbalshaikh <shaikhiqbal80004@gmail.com>
2025-11-02 19:04:59 +0530

    cors updated

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -1,10 +1,8 @@
 import cors from 'cors';
 
-const allowedOrigins = (
-  process.env.NODE_ENV === 'production'
-    ? ['https://expense-tracker-app-pearl-five.vercel.app/']
-    : ['http://localhost:5173']
-).filter(Boolean) as string[];
+const allowedOrigins = [
+  'https://expense-tracker-app-pearl-five.vercel.app/',
+].filter(Boolean) as string[];
 
 const corsOptions = {
   origin: allowedOrigins,
