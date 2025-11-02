3b8001eaa3184c1d7d66cbababa8058f9e8c1071
iqbalshaikh <shaikhiqbal80004@gmail.com>
2025-11-02 18:25:18 +0530

    allowing universale

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -5,9 +5,9 @@ const allowedOrigins = (
     ? [process.env.FRONTEND_URL]
     : ['http://localhost:5173']
 ).filter(Boolean) as string[];
-
+// allowedOrigins
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: ['*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
